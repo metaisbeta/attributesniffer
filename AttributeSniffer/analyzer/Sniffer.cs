e8b7ce802ae7a6541aeb62ca20716a9a9f4f079b
Lydia Braga <lydiaccb@hotmail.com>
2019-01-30 21:48:12 -0200

    Reading files from folder and converting result to json.

---
+++ AttributeSniffer/analyzer/Sniffer.cs
@@ -0,0 +1,27 @@
+﻿using AttributeSniffer.analyzer.classMetrics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AttributeSniffer.analyzer
+{
+    class Sniffer
+    {
+        public String Sniff(string folderPath)
+        {
+            List<ClassMetrics> collectedMetrics = new List<ClassMetrics>();
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.cs"))
+            {
+                FileStream classContent = File.OpenRead(file);
+
+                // Coletar métricas
+            }
+
+            return JsonConvert.SerializeObject(collectedMetrics);
+        }
+    }
+}
---
+++ AttributeSniffer/analyzer/classMetrics/ClassMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeSniffer.analyzer.classMetrics
+{
+    class ClassMetrics
+    {
+        private string className { get; set; }
+        private int numberOfAttributes { get; set; }
+
+        public ClassMetrics(string className, int numberOfAttributes)
+        {
+            this.className = className;
+            this.numberOfAttributes = numberOfAttributes;
+        }
+    }
+}
